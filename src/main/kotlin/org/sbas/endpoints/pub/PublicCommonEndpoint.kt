3137f56c53b85b9891f412a2027b67e728fb882c
김성준 <ted@bitflow.ai>
2023-03-15 09:58:36 +0900

    REST Endpoints stub 2건 작성

---
+++ src/main/kotlin/org/sbas/endpoints/pub/PublicCommonEndpoint.kt
@@ -0,0 +1,89 @@
+package org.sbas.endpoints.pub
+
+import org.eclipse.microprofile.openapi.annotations.Operation
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody
+import org.eclipse.microprofile.openapi.annotations.tags.Tag
+import org.jboss.logging.Logger
+import org.jboss.resteasy.reactive.RestPath
+import org.sbas.entities.info.InfoUser
+import org.sbas.parameters.BaseCodeRequest
+import org.sbas.response.BaseCodeResponse
+import org.sbas.response.EgenCodeMastResponse
+import org.sbas.services.TestUserService
+import javax.annotation.security.PermitAll
+import javax.annotation.security.RolesAllowed
+import javax.inject.Inject
+import javax.ws.rs.GET
+import javax.ws.rs.POST
+import javax.ws.rs.Path
+import javax.ws.rs.core.Context
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
+import javax.ws.rs.core.SecurityContext
+
+@Tag(name = "사용자 API", description = "비 로그인 사용자를 위한 정보조회, 가입요청 및 로그인 API")
+@Path("v1/public/common")
+class PublicCommonEndpoint {
+
+    @Inject
+    lateinit var log: Logger
+
+    @Inject
+    lateinit var security: SecurityContext
+
+    @Operation(summary = "", description = "")
+    @GET
+    @Path("usercommcodes")
+    fun usercommcodes(): Response {
+        return Response.ok().build()
+    }
+
+    @Operation(summary = "", description = "")
+    @GET
+    @Path("egencodes/{param}")
+    fun egencodes(@RestPath param: String): Response {
+        return Response.ok().build()
+    }
+
+    @Operation(summary = "", description = "")
+    @GET
+    @Path("sidos")
+    fun sidos(): Response {
+        return Response.ok().build()
+    }
+
+    @Operation(summary = "", description = "")
+    @GET
+    @Path("guguns/{param}")
+    fun guguns(@RestPath param: String): Response {
+        return Response.ok().build()
+    }
+
+    @Operation(summary = "", description = "")
+    @GET
+    @Path("codes/{param}")
+    fun codes(): Response {
+        return Response.ok().build()
+    }
+
+    @Operation(summary = "", description = "")
+    @GET
+    @Path("download/{param1}/{param2}")
+    fun download(): Response {
+       return Response.ok().build()
+    }
+
+    @Operation(summary = "", description = "")
+    @POST
+    @Path("upload")
+    fun upload(): Response {
+        return Response.ok().build()
+    }
+
+    @Operation(summary = "", description = "")
+    @GET
+    @Path("files/{param}")
+    fun files(): Response {
+        return Response.ok().build()
+    }
+}
---
+++ src/main/kotlin/org/sbas/endpoints/pub/PublicUserEndpoint.kt
@@ -0,0 +1,75 @@
+package org.sbas.endpoints.pub
+
+import org.eclipse.microprofile.openapi.annotations.Operation
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody
+import org.eclipse.microprofile.openapi.annotations.tags.Tag
+import org.jboss.logging.Logger
+import org.sbas.entities.info.InfoUser
+import org.sbas.parameters.BaseCodeRequest
+import org.sbas.response.BaseCodeResponse
+import org.sbas.response.EgenCodeMastResponse
+import org.sbas.services.TestUserService
+import javax.annotation.security.PermitAll
+import javax.annotation.security.RolesAllowed
+import javax.inject.Inject
+import javax.ws.rs.GET
+import javax.ws.rs.POST
+import javax.ws.rs.Path
+import javax.ws.rs.core.Context
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
+import javax.ws.rs.core.SecurityContext
+
+@Tag(name = "사용자 API", description = "비 로그인 사용자를 위한 정보조회, 가입요청 및 로그인 API")
+@Path("v1/public/user")
+class PublicUserEndpoint {
+
+    @Inject
+    lateinit var log: Logger
+
+    @Inject
+    lateinit var security: SecurityContext
+
+    @Operation(summary = "", description = "")
+    @GET
+    @Path("existid")
+    fun existid(): Response {
+        return Response.ok().build()
+    }
+
+    @Operation(summary = "", description = "")
+    @GET
+    @Path("existcellphone")
+    fun existcellphone(): Response {
+        return Response.ok().build()
+    }
+
+    @Operation(summary = "", description = "")
+    @POST
+    @Path("smssend")
+    fun smssend(): Response {
+        return Response.ok().build()
+    }
+
+    @Operation(summary = "", description = "")
+    @POST
+    @Path("confirmsms")
+    fun confirmsms(): Response {
+        return Response.ok().build()
+    }
+
+    @Operation(summary = "", description = "")
+    @POST
+    @Path("requserregt")
+    fun requserregt(): Response {
+        return Response.ok().build()
+    }
+
+    @Operation(summary = "", description = "")
+    @POST
+    @Path("login")
+    fun login(): Response {
+       return Response.ok().build()
+    }
+
+}
