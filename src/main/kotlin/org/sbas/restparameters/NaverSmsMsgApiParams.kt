f2c3cbbd244efb81af3aa55716df17556c79bc0e
김성준 <ted@bitflow.ai>
2023-03-16 19:14:54 +0900

    네이버 SMS 연동 Stub 업데이트

--- src/main/kotlin/org/sbas/restparameters/NaverSmsMsgApiParams.kt
+++ src/main/kotlin/org/sbas/restparameters/NaverSmsMsgApiParams.kt
@@ -1,29 +1,47 @@
 package org.sbas.restparameters
 
 import org.sbas.restresponses.NaverSmsResMsgs
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
 
 data class NaverSmsMsgApiParams (
 
+    @field: NotNull (message = "메시지 타입이 누락되었습니다 (SMS, LMS, MMS)")
     var type: String,
-    var countryCode: String,
+
+    var countryCode: String?,
+
+    @field: NotNull (message = "발신번호가 누락되었습니다")
     var from: String,
-    var subject: String,
-    var contentType: String,
+
+    var subject: String?,
+
+    var contentType: String?,
+
+    @field: NotNull (message = "기본 메시지 내용이 누락되었습니다")
     var content: String,
-    var reserveTime: String,
-    var reserveTimeZone: String,
-    var scheduleCode: String,
-    var messages: List<NaverSmsResMsgs>,
-    var files: List<NaverSmsReqFiles>
+
+    var reserveTime: String?,
+
+    var reserveTimeZone: String?,
+
+    var scheduleCode: String?,
+
+    @field: NotNull (message = "메시지 수신번호 목록이 누락되었습니다")
+    var messages: List<NaverSmsReqMsgs>,
+
+    var files: List<NaverSmsReqFiles>?
+
 )
 
 data class NaverSmsReqMsgs(
-    var subject: String,
-    var content: String,
+    var subject: String?,
+    var content: String?,
+    @field: NotNull (message = "메시지 수신번호가 누락되었습니다.")
     var to: String
 )
 
 data class NaverSmsReqFiles(
-    var name: String,
-    var body: String
+    var name: String?,
+    var body: String?
 )
