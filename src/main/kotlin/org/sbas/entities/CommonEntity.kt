b809f7a8215a26cc6031a5e5612a84846cf50abe
jiseong <jeff3877@gmail.com>
2023-04-04 11:18:24 +0900

    MyEntityListener

--- src/main/kotlin/org/sbas/entities/CommonEntity.kt
+++ src/main/kotlin/org/sbas/entities/CommonEntity.kt
@@ -2,20 +2,21 @@ package org.sbas.entities
 
 import org.hibernate.annotations.CreationTimestamp
 import org.hibernate.annotations.UpdateTimestamp
+import org.sbas.utils.MyEntityListener
 import java.time.Instant
-import javax.persistence.Column
-import javax.persistence.MappedSuperclass
+import javax.persistence.*
 
 @MappedSuperclass
+@EntityListeners(MyEntityListener::class)
 abstract class CommonEntity (
-    @Column(name = "rgst_user_id", nullable = false, length = 10)
+    @Column(name = "rgst_user_id", nullable = false, length = 15)
     var rgstUserId: String? = null, // 등록 사용자 ID
 
     @Column(name = "rgst_dttm", nullable = false, updatable = false)
     @CreationTimestamp
     var rgstDttm: Instant? = null, // 등록 일시
 
-    @Column(name = "updt_user_id", nullable = false, length = 10)
+    @Column(name = "updt_user_id", nullable = false, length = 15)
     var updtUserId: String? = null, // 수정 사용자 ID
 
     @Column(name = "updt_dttm", nullable = false)
---
+++ src/main/kotlin/org/sbas/utils/MyEntityListener.kt
@@ -0,0 +1,29 @@
+package org.sbas.utils
+
+import org.eclipse.microprofile.jwt.JsonWebToken
+import org.sbas.entities.CommonEntity
+import javax.enterprise.context.ApplicationScoped
+import javax.inject.Inject
+import javax.persistence.PrePersist
+import javax.persistence.PreUpdate
+
+@ApplicationScoped
+class MyEntityListener {
+
+    @Inject
+    private lateinit var jsonWebToken: JsonWebToken
+    @PrePersist
+    fun prePersist(entity: CommonEntity) {
+        // Entity가 영속성 컨텍스트에 저장되기 전에 호출됩니다.
+        // 이전에 수행할 로직을 여기에 작성합니다.
+        entity.rgstUserId = jsonWebToken.name
+        entity.updtUserId = jsonWebToken.name
+    }
+
+    @PreUpdate
+    fun preUpdate(entity: CommonEntity) {
+        // Entity가 업데이트되기 전에 호출됩니다.
+        // 이전에 수행할 로직을 여기에 작성합니다.
+        entity.updtUserId = jsonWebToken.name
+    }
+}
