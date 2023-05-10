a808464121b577e17f3f772705fc106227cbcae8
jiseong <jeff3877@gmail.com>
2023-05-10 22:59:57 +0900

    환자 목록 조회 쿼리 수정중...

--- src/main/kotlin/org/sbas/repositories/InfoRepository.kt
+++ src/main/kotlin/org/sbas/repositories/InfoRepository.kt
@@ -18,51 +18,18 @@ class InfoPtRepository : PanacheRepositoryBase<InfoPt, String> {
     }
 
     fun findInfoPtList(): List<InfoPtSearchDto> {
-        val query = "select new org.sbas.dtos.InfoPtSearchDto(a.ptId, b.id.bdasSeq, a.ptNm, a.gndr, " +
+        //TODO
+        val query = "select new org.sbas.dtos.InfoPtSearchDto(a.ptId, max(b.id.bdasSeq), a.ptNm, a.gndr, " +
                 "a.dstr1Cd, (select cdNm from BaseCode where id.cdId = a.dstr1Cd), a.dstr2Cd, (select cdNm from BaseCode where id.cdId = a.dstr2Cd), " +
-                "ba.hospId, '', a.mpno, a.natiCd, a.bedStatCd, a.bedStatNm, b.updtDttm, " +
-                "b.ptTypeCd, b.svrtTypeCd, b.undrDsesCd, " +
+                "max(ba.hospId), '', a.mpno, a.natiCd, fn_get_bed_asgn_stat(a.ptId, max(b.id.bdasSeq)), '', max(b.updtDttm), " +
+                "max(b.ptTypeCd), max(b.svrtTypeCd), max(b.undrDsesCd), " +
                 "EXTRACT(year FROM age(CURRENT_DATE, to_date(case a.rrno2 when '3' then concat('20',a.rrno1) when '4' then concat('20',a.rrno1) else concat('19',a.rrno1) end, 'yyyyMMdd')))) " +
                 "FROM InfoPt a " +
                 "left join BdasReq b on a.ptId = b.id.ptId " +
                 "left join BdasAdms ba on b.id.bdasSeq = ba.id.bdasSeq " +
-//                "where b.id.bdasSeq in (select max(id.bdasSeq) as bdasSeq from BdasReq group by id.ptId) " +
-                "order by b.updtDttm desc"
+                "group by a.ptId " +
+                "order by max(b.updtDttm) desc"
         return getEntityManager().createQuery(query, InfoPtSearchDto::class.java).resultList
-//        val query = "select a.pt_id, " +
-//                "b.bdas_seq, " +
-//                "a.pt_nm, " +
-//                "a.gndr, " +
-//                "a.dstr_1_Cd, " +
-//                "(select cd_nm from base_code where cd_id = a.dstr_1_cd) as dstr_1_cd_nm, " +
-//                "a.dstr_2_cd, " +
-//                "(select cd_nm from base_code where cd_id = a.dstr_2_cd) as dstr_2_cd_nm, " +
-//                "ba.hosp_id, " +
-//                "'' as hosp_nm, " +
-//                "a.mpno, " +
-//                "a.nati_cd, " +
-//                "fn_get_bed_asgn_stat(a.pt_id, b.bdas_seq) as statCd, " +
-//                "'' as statCdNm, " +
-//                "b.updt_dttm, " +
-//                "b.pt_type_cd, " +
-//                "b.svrt_type_cd, " +
-//                "b.undr_dses_cd, " +
-//                "EXTRACT(year FROM age(CURRENT_DATE, to_date(case a.rrno_2 " +
-//                "when '3' then concat('20', a.rrno_1) " +
-//                "when '4' then concat('20', a.rrno_1) " +
-//                "else concat('19', a.rrno_1) end, 'yyyyMMdd'))) " +
-//                "FROM info_pt a " +
-//                "left join (select bdas_seq, pt_id, updt_dttm, pt_type_cd, svrt_type_cd, undr_dses_cd " +
-//                "from bdas_req " +
-//                "where bdas_seq in (select max(bdas_seq) from bdas_req group by pt_id)) b " +
-//                "on a.pt_id = b.pt_id " +
-//                "left outer join bdas_adms ba on b.bdas_seq = ba.bdas_seq " +
-//                "order by b.updt_dttm desc"
-//
-//        return find(query).project(InfoPtSearchDto::class.java).list()
-//        val criteriaBuilder = getEntityManager().criteriaBuilder
-//        criteriaBuilder.createQuery()
-//        return getEntityManager().createNativeQuery(query, InfoPtSearchDto::class.java).resultList as List<InfoPtSearchDto>
     }
 
     fun findInfoPt(): MutableList<Any?>? {
