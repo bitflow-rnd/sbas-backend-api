05cfed7f01b77625bfff3ec18b12d5232d6e662b
jiseong <jeff3877@gmail.com>
2023-09-06 15:04:35 +0900

    secret key 재생성

--- src/main/kotlin/org/sbas/restclients/NaverOcrRestClient.kt
+++ src/main/kotlin/org/sbas/restclients/NaverOcrRestClient.kt
@@ -14,17 +14,16 @@ import javax.ws.rs.Path
  * Naver Clova OCR API를 처리하는 클라이언트
  */
 //@Path("custom/v1/21302")
-@Path("custom/v1/21959") // template ocr 주소 사용
+@Path("custom/v1/24838") // template ocr 주소 사용
 @RegisterClientHeaders(NaverOcrHeaderFactory::class)
 @RegisterRestClient
 interface NaverOcrRestClient {
-
     /**
      * 파일 인식 요청
      */
     @POST
 //    @Path("8e453b0208e971dcab58a1d1aad960e58e9b8652dc362baa6a1897e94561bfc8/general")
-    @Path("f7652e3870798fb5530893a27d8c57203432063092b37cce8efbd8a59ce17fec/infer") // template ocr 주소 사용
+    @Path("ba0fc5ee279c1a065730ff9b4936b69f2992b2f637ab4af8c61f3bc6346cdefb/infer") // template ocr 주소 사용
     fun recognize(@Valid param: NaverOcrApiParams): NaverOcrApiResponse
 
 }
