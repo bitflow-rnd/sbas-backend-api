6725c84b1f9c6110dcffcf5bfe349761c6c9fe5c
김성준 <ted@bitflow.ai>
2023-09-08 02:51:14 +0900

    채팅방 URL 및 구조 변경

---
+++ src/main/kotlin/org/sbas/utils/TalkRoomMod.kt
@@ -0,0 +1,89 @@
+package org.sbas.utils
+
+import io.vertx.core.json.JsonArray
+import io.vertx.core.json.JsonObject
+import kotlinx.coroutines.*
+import org.jboss.logging.Logger
+import org.sbas.entities.talk.TalkMsg
+import org.sbas.entities.talk.TalkUser
+import org.sbas.repositories.TalkMsgRepository
+import org.sbas.repositories.TalkUserRepository
+import javax.inject.Inject
+import javax.websocket.*
+import javax.websocket.server.PathParam
+import javax.websocket.server.ServerEndpoint
+
+
+@ServerEndpoint("/chat-rooms/room/{tkrmId}")
+class TalkRoomMod {
+
+    companion object {
+        private val chatSockets = mutableMapOf<String, TalkRoomMod>() // WebSocket 연결을 관리할 Map
+        private lateinit var talkMsg: MutableList<TalkMsg>
+    }
+
+    private lateinit var session: Session // WebSocket 세션
+    private lateinit var tkrmId: String // 채팅방 ID
+
+    @Inject
+    lateinit var log: Logger
+
+    @Inject
+    lateinit var talkMsgRepository: TalkMsgRepository
+
+    @Inject
+    lateinit var talkUserRepository: TalkUserRepository
+
+    @OnOpen
+    fun onOpen(session: Session, @PathParam("tkrmId") tkrmId: String) {
+
+        this.session = session
+        this.tkrmId = tkrmId
+
+        updateTalkMsg(tkrmId)
+
+        val sendObject = JsonArray.of(talkMsg).toString()
+        session.asyncRemote.sendText(sendObject)
+
+        chatSockets[tkrmId] = this // WebSocket 연결을 Map에 추가
+    }
+
+    @OnMessage
+    fun onMessage(session: Session, data: String, @PathParam("tkrmId") tkrmId: String) {
+        var addMsg: TalkMsg
+        val otherUsers: MutableList<TalkUser>
+        log.debug("data" + data)
+        val idx = data.indexOf("|")
+        val userId = data.substring(0, idx)
+        val message = data.substring(idx+1)
+        runBlocking(Dispatchers.IO) {
+            addMsg = talkMsgRepository.insertMessage(message, tkrmId, userId)
+            otherUsers = talkUserRepository.findOtherUsersByTkrmId(tkrmId, userId) as MutableList<TalkUser>
+        }
+
+        chatSockets.values // 모든 WebSocket 연결에 메시지 전송
+            .filter { it.tkrmId == tkrmId }
+            .forEach {
+                it.session.asyncRemote.sendText(JsonObject.mapFrom(addMsg).toString())
+            }
+
+        otherUsers.forEach{
+            session.asyncRemote.sendText(it.id?.userId)
+        }
+
+    }
+
+    @OnClose
+    fun onClose(session: Session, @PathParam("userId") userId: String) {
+        chatSockets.remove(userId) // WebSocket 연결을 Map에서 제거
+    }
+
+    private fun updateTalkMsg(tkrmId: String) {
+        val resultList = runBlocking {
+            withContext(Dispatchers.IO) {
+                talkMsgRepository.findChatDetail(tkrmId)
+            }
+        } as MutableList<TalkMsg>
+        talkMsg = resultList
+    }
+}
