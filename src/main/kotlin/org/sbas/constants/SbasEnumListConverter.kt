20af204acc1879243492c08e8b90e06fac4e3053
jiseong <jeff3877@gmail.com>
2023-10-30 14:32:38 +0900

    누락 파일 추가

---
+++ src/main/kotlin/org/sbas/constants/SbasEnumListConverter.kt
@@ -0,0 +1,64 @@
+package org.sbas.constants
+
+import org.sbas.constants.enums.SbasEnum
+import org.sbas.constants.enums.UndrDsesCd
+import javax.persistence.AttributeConverter
+
+open class SbasEnumListConverter<T : SbasEnum>(private val enumClass: Class<T>) : AttributeConverter<List<T>, String> {
+
+    override fun convertToDatabaseColumn(attribute: List<T>?): String? {
+        if (attribute.isNullOrEmpty()) {
+            return null
+        }
+        return attribute.joinToString(";")
+    }
+
+    override fun convertToEntityAttribute(dbData: String?): List<T>? {
+        return dbData?.split(";")?.mapNotNull { code -> findEnumByCdNm(code) }
+    }
+
+    private fun findEnumByCdNm(code: String): T? {
+        return enumClass.enumConstants.find { it.code == code }
+    }
+}
+
+class UndrDsesCdConverter : SbasEnumListConverter<UndrDsesCd>(UndrDsesCd::class.java)
+
+
+//class UndrDsesCdConverter : AttributeConverter<List<SbasEnum>, String> {
+//
+//    override fun convertToDatabaseColumn(attribute: List<SbasEnum>?): String? {
+//        if (attribute.isNullOrEmpty()) {
+//            return null
+//        }
+//        return attribute.joinToString(";")
+//    }
+//
+//    override fun convertToEntityAttribute(dbData: String?): List<SbasEnum>? {
+//        return dbData?.split(";")?.map { findEnumByCdNm(it) }
+//    }
+//
+//    private fun findEnumByCdNm(code: String): UndrDsesCd {
+//        return UndrDsesCd.valueOf(code)
+//    }
+//
+//}
+//
+//class PtTypeCdConverter : AttributeConverter<List<SbasEnum>, String> {
+//
+//    override fun convertToDatabaseColumn(attribute: List<SbasEnum>?): String? {
+//        if (attribute.isNullOrEmpty()) {
+//            return null
+//        }
+//        return attribute.joinToString(";")
+//    }
+//
+//    override fun convertToEntityAttribute(dbData: String?): List<SbasEnum>? {
+//        return dbData?.split(";")?.map { findEnumByCdNm(it) }
+//    }
+//
+//    private fun findEnumByCdNm(code: String): PtTypeCd {
+//        return PtTypeCd.valueOf(code)
+//    }
+//
+//}
