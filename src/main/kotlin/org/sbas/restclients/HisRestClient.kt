a472442a1a3b7495fda6cdc2491458a39ee43e18
jiseong <jeff3877@gmail.com>
2024-06-18 17:02:27 +0900

    경북대, 칠곡 RestClient 추가

--- src/main/kotlin/org/sbas/restclients/HisRestClient.kt
+++ src/main/kotlin/org/sbas/restclients/HisRestClient.kt
@@ -8,13 +8,26 @@ import org.sbas.restresponses.HisApiResponse
 /**
  * HIS API를 처리하는 클라이언트
  */
-@RegisterRestClient(baseUri = "https://mcaredoctor.fatima.or.kr/qapiplus-dev/api/")
+@RegisterRestClient(baseUri = "https://mcaredoctor.fatima.or.kr/qapiplus-dev/api")
 interface FatimaHisRestClient {
 
   @POST
   @Path("get_svr_mntr_info_fnu/v1")
   fun getFatimaSvrtMntrInfo(body: HisRestClientRequest): HisApiResponse
 }
+
+@RegisterRestClient(baseUri = "https://sbas.knuh.kr/qapi/api/")
+interface KnuhHisRestClient {
+
+  @POST
+  @Path("get_svr_mntr_info_knuh/v1")
+  fun getKnuhSvrtMntrInfo(body: HisRestClientRequest): HisApiResponse
+
+  @POST
+  @Path("get_svr_mntr_info_knuch/v1")
+  fun getKnuchSvrtMntrInfo(body: HisRestClientRequest): HisApiResponse
+}
+
 
 data class HisRestClientRequest(
   val pid: String,
