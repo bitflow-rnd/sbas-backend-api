caf4aa9742384d5988d94bc75a25ea4df87e1a26
김학성 <hak9501@gmail.com>
2024-10-07 16:29:46 +0900

    채팅방 생성 시 상대 이름 삭제

--- src/main/kotlin/org/sbas/dtos/TalkDto.kt
+++ src/main/kotlin/org/sbas/dtos/TalkDto.kt
@@ -28,7 +28,6 @@ data class TalkMsgDto(
 data class RegTalkRoomDto(
   var id: String, // 유저 ID
   var userId: String, // 초대받는 유저 ID
-  var userNm: String,
 ) {
   fun toEntity(tkrmId: String): TalkRoom {
     return TalkRoom(
